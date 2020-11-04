38176108d88273e325f2982b83a1457f7ee1cd16
BoB <vinniethepooloo@protonmail.com>
2020-11-04 15:02:13 +0500

    added material-ui components still need to fix them not rendering

---
+++ apps/store/src/api/index.ts
@@ -0,0 +1,50 @@
+class GameEntry {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  rating: number;
+
+  constructor(id: string, name: string, image: string, description: string, price: number, rating: number) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.description = description;
+    this.price = price;
+    this.rating = rating;
+  }
+}
+
+export const games = [
+  new GameEntry(
+    "settlers-in-the-can",
+    "Settlers in the Can",
+    "./assets/beans.jpg",
+    "Help your bug family claim the best real estate in a spilled can of beans",
+    35,
+    Math.random(),
+  ),
+  new GameEntry(
+    "chess-pie",
+    "Chess Pie",
+    "./assets/chess.jpg",
+    "A circular game of Chess that you can eat as you play",
+    15,
+    Math.random(),
+  ),
+  new GameEntry(
+    "purrfection",
+    "Purrfection",
+    "./assets/cat.jpg",
+    "A cat grooming contest goes horrible wrong",
+    45,
+    Math.random(),
+  ),
+];
+
+export function getGame( id: string ) {
+  games.find((game) => {
+    game.id === id;
+  });
+}
--- apps/store/src/main.tsx
+++ apps/store/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './app/app';
+import { App } from './app/app';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.querySelector('#root')
 );
